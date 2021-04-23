ed0181712a10d33d0767e42fa18a03ad93c99360
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2021-04-23 09:09:02 +0200

    Add project files.

---
+++ Esercizio2ThreadconAsyncAwait/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Esercizio2ThreadconAsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("numero corridori: ");
+            int n = int.Parse(Console.ReadLine());
+            List<string> partecipanti = new List<string>();
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"come si chiama {i}:");
+                string name = Console.ReadLine();
+                partecipanti.Add(name);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                corsa(partecipanti[i]);
+
+            }
+
+            Console.ReadLine();
+
+        }
+        private static async Task corsa(string name)
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i <= 100; i++)
+                {
+                    Console.WriteLine ($"il corridore {name} ha percorso {i} ");
+                }
+                Console.WriteLine ($"la gara è stata finita da: {name}");
+
+            });
+       
+        
+            
+
+
+        }
+
+    }
+}
+    
